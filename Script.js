2c40bc24d307184b15ece4493ff9d2351b26cf7d
Rifat654 <141209933+Rifat654@users.noreply.github.com>
2024-08-24 15:44:21 +0600

    added

---
+++ Script.js
@@ -0,0 +1,11 @@
+// Custom css for tailwind
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        fast: "rgb(255,132,54)",
+        last: "rgb(255,7,3)",
+      },
+    },
+  },
+};
